b307ad917224a855ebb3eaad2aa9c6a8930c3da4
Stella Zhu <stellazhu@gatech.edu>
2020-05-09 03:46:12 -0400

    v2

--- Restaurant_Searcher.py
+++ Restaurant_Searcher.py
@@ -4,11 +4,10 @@ author: Stella Zhu
 
 import requests
 from US_city_extracter import city_locator
+import csv
 
 google_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 api_key = "AIzaSyCQ1URNVCMsTp3bZAuIP0WG8Ljto28WEko"
-
-us_county_code = "us"
 
 
 def restaurants_searcher(state_name, city_name):
@@ -18,20 +17,6 @@ def restaurants_searcher(state_name, city_name):
     :param city_name: case-insensitive city_name
     :return: a dictionary with restaurants as keys and opening status as values
     """
-    if " " not in state_name:
-        state_name = state_name[0].upper() + state_name[1:].lower()
-    else:
-        state_name = state_name.split()
-        state_name[0] = state_name[0][0].upper() + state_name[0][1:].lower()
-        state_name[1] = state_name[1][0].upper() + state_name[1][1:].lower()
-        state_name = " ".join(state_name)
-    if " " not in city_name:
-        city_name = city_name[0].upper() + city_name[1:].lower()
-    else:
-        city_name = city_name.split()
-        city_name[0] = city_name[0][0].upper() + city_name[0][1:].lower()
-        city_name[1] = city_name[1][0].upper() + city_name[1][1:].lower()
-        city_name = " ".join(city_name)
     state_city_location_dict = city_locator()
     location_tup = state_city_location_dict[state_name][city_name]
     city_url = google_url + "location=" + location_tup[0] + "," + location_tup[1] \
@@ -52,6 +37,29 @@ def restaurants_searcher(state_name, city_name):
     # print(city_url)
     return result
 
+def restaurants_csv_generater(state_name, city_name):
+    if " " not in state_name:
+        state_name = state_name[0].upper() + state_name[1:].lower()
+    else:
+        state_name = state_name.split()
+        state_name[0] = state_name[0][0].upper() + state_name[0][1:].lower()
+        state_name[1] = state_name[1][0].upper() + state_name[1][1:].lower()
+        state_name = " ".join(state_name)
+    if " " not in city_name:
+        city_name = city_name[0].upper() + city_name[1:].lower()
+    else:
+        city_name = city_name.split()
+        city_name[0] = city_name[0][0].upper() + city_name[0][1:].lower()
+        city_name[1] = city_name[1][0].upper() + city_name[1][1:].lower()
+        city_name = " ".join(city_name)
+
+    with open("restaurants.csv", "w") as fout:
+        fieldnames = ['city', 'restaurant', 'opening_status']
+        dw = csv.DictWriter(fout, fieldnames)
+        dw.writeheader()
+        restaurants_info = restaurants_searcher(state_name, city_name)
+        dw.writerows([{fieldnames[0]: city_name, fieldnames[1]: restaurant,
+                       fieldnames[2]: restaurants_info[restaurant]} for restaurant in restaurants_info])
 
 if __name__ == "__main__":
-    print(restaurants_searcher("new york", "washingtonville"))
+    restaurants_csv_generater("new york", "washingtonville")
